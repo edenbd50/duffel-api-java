860f3c07258bcd12eb944d5b26b1c5ae2ccbd7c2
Eden <edenbd50@gmail.com>
2023-12-07 03:30:19 +0200

    Add PrivateFare and IdentityDocumentType support

---
+++ src/main/java/com/duffel/model/response/IdentityDocumentType.java
@@ -0,0 +1,12 @@
+package com.duffel.model.response;
+
+/**
+ * Type of baggage.
+ */
+public enum IdentityDocumentType {
+
+    passport,
+    known_traveler_number,
+    passenger_redress_number,
+
+}
